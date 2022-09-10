976a5cbdf87574f34b7103340f051187ffc39b1c
Sangah Yoon <sayoon26@gmail.com>
2022-09-10 20:17:20 +0900

    login dialog 관련 UI 1차 구현

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const liveClockView = document.querySelector("#live-clock");
 
+const userDisplayer = document.querySelector("#user-displayer");
+
+const loginDialogMask = document.querySelector("#login-dialog-mask");
+const userLoginButton = document.querySelector(
+  '#login-dialog input[type="button"]'
+);
+
+let isLoggedIn = false;
+let userName = "";
+
 const refreshLiveClock = function () {
   const currentDateTime = new Date();
 
@@ -15,5 +25,33 @@ const refreshLiveClock = function () {
 
   setInterval(refreshLiveClock, 1000);
 };
+
+const userDisplayerClickHandler = function () {
+  if (isLoggedIn) {
+    // if (confirm("로그아웃 하시겠습니까?")) {
+    //   // localstorage 지우기
+    // }
+  } else {
+    loginDialogMask.classList.remove("hidden");
+  }
+};
+
+const userLoginButtonClickHandler = function () {
+  const userId = document.querySelector(
+    '#login-dialog input[type="text"]'
+  ).value;
+
+  if (userId == "" || userId == null) {
+    alert("ID를 입력해주세요.");
+  } else {
+    // login하고
+
+    // login dialog 숨기기
+    loginDialogMask.classList.add("hidden");
+  }
+};
+
+userDisplayer.addEventListener("click", userDisplayerClickHandler);
+userLoginButton.addEventListener("click", userLoginButtonClickHandler);
 
 refreshLiveClock();
