87edd6cf90c092cae0d18635e13f4aa780d53d67
Sangah Yoon <sayoon26@gmail.com>
2022-09-10 19:57:41 +0900

    월, 일, 시, 분, 초 2자리 아닐 경우 0 추가

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const refreshLiveClock = function () {
   const currentDateTime = new Date();
 
   const year = currentDateTime.getFullYear();
-  const month = currentDateTime.getMonth();
-  const day = currentDateTime.getDay();
+  const month = currentDateTime.getMonth().toString().padStart(2, "0");
+  const day = currentDateTime.getDay().toString().padStart(2, "0");
 
-  const hour = currentDateTime.getHours();
-  const minute = currentDateTime.getMinutes();
-  const second = currentDateTime.getSeconds();
+  const hour = currentDateTime.getHours().toString().padStart(2, "0");
+  const minute = currentDateTime.getMinutes().toString().padStart(2, "0");
+  const second = currentDateTime.getSeconds().toString().padStart(2, "0");
 
   liveClockView.innerHTML = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
 
